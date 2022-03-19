8fb644a49d0539e93f0c2cbbd88a0c53bca16eb5
Luke Thompson <endeavour9@gmail.com>
2022-03-19 21:05:39 +1100

    Complete build and deploy scripts using docker exec to prevent redownloading crates every build.

--- compile-lambda.sh
+++ compile-lambda.sh
@@ -1,10 +1,8 @@
 set -euo pipefail
 
-cd rust-lambda-compiler
-docker build -t rust-lambda-compiler .
-cd ..
+docker run -it --rm -v $(pwd):/code --name rust-lambda-compiler -d --entrypoint /bin/bash rust-lambda-compiler || echo "Already running"
+docker exec -it rust-lambda-compiler cargo build --release --target aarch64-unknown-linux-gnu
 
-docker run -it --rm -v $(pwd):/code rust-lambda-compiler build --release --target aarch64-unknown-linux-gnu
 rm -rf ./out-deploy
 mkdir -p ./out-deploy
 cp ./target/aarch64-unknown-linux-gnu/release/rusty-lambda ./out-deploy/bootstrap
