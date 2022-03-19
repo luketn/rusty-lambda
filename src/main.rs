7cf47e7bdfad28e34d9e44dee8cbdccbfb9ad70d
Luke Thompson <endeavour9@gmail.com>
2022-03-19 22:03:23 +1100

    Cleaner code with let Some() https://services.mycodefu.com/rust/rusty-lambda

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use lambda_runtime::{service_fn, LambdaEvent, Error};
-use serde_json::{json, Value};
+use lambda_http::{service_fn, Error, RequestExt, Request, IntoResponse};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let func = service_fn(func);
-    lambda_runtime::run(func).await?;
+    lambda_http::run(func).await?;
     Ok(())
 }
 
-async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+async fn func(event: Request) -> Result<impl IntoResponse, Error> {
+    let query_string_parameters = event.query_string_parameters();
+    let first_name = query_string_parameters.first("firstName");
 
-    Ok(json!({ "message": format!("Hi, there, {}!", first_name) }))
+    if let Some(first_name) = first_name {
+        Ok(format!("Hello, {first_name}!").into_response())
+    } else {
+        Ok("Hello, rusty world! Add a query parameter 'firstName' for a personalised greeting.".into_response())
+    }
 }
