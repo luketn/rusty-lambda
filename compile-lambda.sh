297d7e6a5149a574d1dee95c1d74730081367a2d
Luke Thompson <endeavour9@gmail.com>
2022-03-20 15:55:35 +1100

    force using arm64 and qemu

--- compile-lambda.sh
+++ compile-lambda.sh
@@ -1,6 +1,6 @@
 set -euo pipefail
 
-docker run --rm -v $(pwd):/code --name rust-lambda-compiler luketn/rust-lambda-compiler build --release --target aarch64-unknown-linux-gnu
+docker run --rm -v $(pwd):/code --platform linux/arm64 --name rust-lambda-compiler luketn/rust-lambda-compiler build --release --target aarch64-unknown-linux-gnu
 #docker run --rm -v $(pwd):/code --name rust-lambda-compiler luketn/rust-lambda-compiler || echo "Already running"
 #docker exec rust-lambda-compiler cargo build --release --target aarch64-unknown-linux-gnu
 
